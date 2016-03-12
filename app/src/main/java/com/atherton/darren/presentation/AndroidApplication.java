89e00005643692ebdbd4aa4755ab895533427ed6
darrenatherton <darren.atherton49@yahoo.com>
2016-03-12 01:04:18 +0000

    adds Timber gradle dependency and plants just a debug tree for now

--- app/src/main/java/com/atherton/darren/presentation/AndroidApplication.java
+++ app/src/main/java/com/atherton/darren/presentation/AndroidApplication.java
@@ -2,10 +2,13 @@ package com.atherton.darren.presentation;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class AndroidApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree());
     }
 }
