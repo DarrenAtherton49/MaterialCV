e3c3f8d77148c2e54008cf5d4f14fc47dcb54fe4
darrenatherton <darren.atherton49@yahoo.com>
2016-04-05 21:45:31 +0100

    adds RUNTIME retention to perActivity annotation

--- app/src/main/java/com/atherton/darren/presentation/injection/scope/PerActivity.java
+++ app/src/main/java/com/atherton/darren/presentation/injection/scope/PerActivity.java
@@ -1,7 +1,11 @@
 package com.atherton.darren.presentation.injection.scope;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import javax.inject.Scope;
 
 @Scope
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PerActivity {
 }
